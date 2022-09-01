e2387eba857593ac378dadcb937e957729522f3d
Rodrigo José <74773266+RodrigoJose-Dev@users.noreply.github.com>
2022-09-01 13:45:06 -0300

    criando header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "../styles/variables.sass";
+import "../styles/components/navbar.sass";
+
+const Navbar = () => {
+    return (
+        <div id="header">
+            <header>
+                <h1>Pokedex</h1>
+            </header>
+        </div>
+    );
+};
+
+export default Navbar;
